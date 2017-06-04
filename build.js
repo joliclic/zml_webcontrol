4dcc95e00de82a1fa5988ae39a5de1d711622451
Nicolas Martin <joliclic@gmail.com>
2017-06-04 12:46:14 +0200

    create a build system, nodejs based. Css are now built from a Less file

---
+++ build.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var path = require('path');
+var fs = require('fs');
+//var fse = require('fs-extra');
+//var util = require('util');
+//var Mold = require('./nodelibs/mold');
+var less = require('less');
+
+var cfg = {
+    compressCSS: false,
+    
+    src_dir: path.join('.', 'src'),
+    
+    target_dir: path.join('.', 'www'),
+};
+
+function compile_css(aSrc, aDest) {
+    var css = fs.readFileSync(aSrc, 'utf8');
+    var options = {
+        compress: cfg.compressCSS,
+        paths: path.dirname(aSrc)
+    };
+    
+    less.render(css, options, function(aError, aOutput) {
+        //console.log(css);
+        if (aError) {
+            less.writeError(aError, {color: true});
+            //console.log('error less.render: ' + aError);
+            return;
+        }
+        
+        fs.writeFileSync(aDest, aOutput.css, {encoding: 'utf8'});
+        console.log('css file created: ' + aDest);
+    });
+}
+
+var cssSrc = path.join(cfg.src_dir, 'zml.less');
+var cssDest = path.join(cfg.target_dir, 'zml.css');
+compile_css(cssSrc, cssDest);
+
